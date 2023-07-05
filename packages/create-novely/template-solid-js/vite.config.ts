fbd62ce6b57495b519b5d4632788a9d82eeeaa39
Artemiy Schukin <shuckinart@yandex.ru>
2023-07-05 14:41:28 +0300

    update template

--- packages/create-novely/template-solid-js/vite.config.ts
+++ packages/create-novely/template-solid-js/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import { default as solidPlugin } from 'vite-plugin-solid'
+import { plugin as pluginNvl } from '@novely/vite-plugin-nvl'
 
 export default defineConfig(() => {
   return {
     plugins: [
+      /**
+       * Plugin that powers SolidJS
+       */
       solidPlugin({
         babel: {
           babelrc: false,
@@ -15,6 +19,10 @@ export default defineConfig(() => {
         hot: false,
         ssr: false,
       }),
+      /**
+       * Plugin for special story file format
+       */
+      pluginNvl
     ],
     build: {
       cssCodeSplit: false,
@@ -23,6 +31,9 @@ export default defineConfig(() => {
     esbuild: {
       charset: 'utf8',
     },
+    /**
+     * Games is hosted on different resources, sometimes relative path is required
+     */
     base: './'
   }
 })
