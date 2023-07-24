5b4e17455fa05f7cc9161c3d2125a2370e4ad795
Artemiy Schukin <schukinart@yandex.ru>
2023-07-24 12:01:35 +0300

    update

--- packages/solid-renderer/build.js
+++ packages/solid-renderer/build.js
@@ -1,8 +1,10 @@
-import { build } from 'esbuild';
+import * as esbuild from 'esbuild';
+import * as lightning from 'lightningcss';
+import * as fs from 'fs/promises';
 
 const dev = process.argv.at(2) === '--watch';
 
-build({
+const context = await esbuild.context({
 	entryPoints: ['./src/index.ts', './src/styles/index.css'],
 	external: ['solid-js'],
 	charset: 'utf8',
@@ -14,5 +16,37 @@ build({
 		'.js': '.jsx',
 	},
 	bundle: true,
-	watch: dev,
+	plugins: [
+		{
+			name: 'css',
+			setup(build) {
+				build.onLoad({ filter: /\.css$/ }, async (args) => {
+					const contents = await fs.readFile(args.path, 'utf-8');
+
+					const { code } = lightning.transform({
+						code: Buffer.from(contents),
+						minify: false,
+						sourceMap: false,
+						targets: {
+							safari: (12 << 16) | (0 << 8),
+						},
+					});
+
+					const css = code.toString('utf-8');
+
+					return {
+						contents: css,
+						loader: 'css',
+					}
+				})
+			}
+		}
+	]
 });
+
+if (dev) {
+	context.watch()
+} else {
+	await context.rebuild();
+	context.dispose();
+}
