cf6dd46f84dc4711cf681a699027126305eb83d3
Artemiy Schukin <shuckinart@yandex.ru>
2023-08-19 10:00:04 +0300

    remove unicorn & fix control panel

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,18 +29,7 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
-        "unicorn/prevent-abbreviations": "off",
-        "unicorn/no-null": "off",
-        "unicorn/prefer-switch": "off",
-        "unicorn/consistent-function-scoping": "off",
-        "unicorn/no-array-reduce": "off",
-        "unicorn/no-nested-ternary": "off",
-        "unicorn/prefer-ternary": "off",
-        "unicorn/filename-case": "off",
-        "unicorn/no-array-for-each": "off",
-        "unicorn/prefer-number-properties": "off",
-        "unicorn/consistent-destructuring": "off",
-        "unicorn/prefer-spread": "off",
+        "@typescript-eslint/no-unused-vars": "off",
         "@typescript-eslint/no-explicit-any": "off",
     }
 }
