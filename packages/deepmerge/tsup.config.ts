7f430701d63d03d3d7d18c9cd4cb11d81e55e395
Artemiy Schukin <shuckinart@yandex.ru>
2023-08-30 17:11:20 +0300

    add deepmerge (copied and modified, see license)

--- packages/deepmerge/tsup.config.ts
+++ packages/deepmerge/tsup.config.ts
@@ -3,15 +3,12 @@ import { defineConfig } from 'tsup';
 export default defineConfig((options) => {
 	return {
 		entry: ['src/index.ts'],
-		external: ['@novely/t9n'],
-		noExternal: ['deepmerge', 'klona'],
 		sourcemap: true,
 		target: 'es2022',
-		format: ['esm', 'iife'],
+		format: ['esm'],
 		bundle: true,
 		minify: false,
 		dts: true,
 		watch: options.watch,
-		globalName: 'Novely',
 	};
 });
