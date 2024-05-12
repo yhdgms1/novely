123bf82172f8c362225ebfc5f70d395502a11c45
Artemiy Schukin <shuckinart@yandex.ru>
2024-05-12 19:31:34 +0300

    bundle particles

--- packages/particles/tsup.config.ts
+++ packages/particles/tsup.config.ts
@@ -4,11 +4,13 @@ export default defineConfig((options) => {
 	return {
 		entry: ['src/index.ts'],
 		external: ['@novely/core'],
+		noExternal: ['@tsparticles/engine', '@tsparticles/slim'],
 		sourcemap: true,
 		target: 'es2022',
 		format: ['esm'],
 		minify: true,
 		bundle: true,
+		splitting: false,
 		dts: options.dts,
 		watch: options.watch,
 	};
