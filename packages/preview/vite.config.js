fb3875627dda29bc6bf608c55553ea3ec0961f4e
Artemiy Schukin <shuckinart@yandex.ru>
2024-08-21 21:03:48 +0300

    cleanup preview

--- packages/preview/vite.config.js
+++ packages/preview/vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite';
-import { default as solidPlugin } from 'vite-plugin-solid';
 import { novelyPlugin } from '@novely/vite-plugin-novely';
 
 export default defineConfig({
 	plugins: [
-		solidPlugin({
-			babel: {
-				babelrc: false,
-				browserslistConfigFile: false,
-				configFile: false,
-				highlightCode: false,
-				plugins: [],
-			},
-			hot: false,
-		}),
 		novelyPlugin(),
 	],
 	build: {
