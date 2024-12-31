af47b213849ec24704d48d73d3efaa21699c0624
Artemiy Schukin <shuckinart@yandex.ru>
2024-12-31 11:03:36 +0300

    mood object

--- packages/solid-renderer/src/custom-actions/mood.ts
+++ packages/solid-renderer/src/custom-actions/mood.ts
@@ -1,14 +1,16 @@
-import type { CustomHandler, ValidAction } from '@novely/core';
+import type { CustomHandler, Lang, ValidAction } from '@novely/core';
 import { useContextState } from '../context-state';
 
 const SET_MOOD = Symbol();
 
-const setMood = (mood: string) => {
-	const handler: CustomHandler = ({ contextKey, clear }) => {
+const setMood = (mood: string | Record<Lang, string>) => {
+	// todo: provive `templateReplace` function from core and use it there
+
+	const handler: CustomHandler = ({ contextKey, lang, clear }) => {
 		const ctx = useContextState(contextKey);
 
 		if (mood === "''" || mood === '""') ctx.setKey('mood', '');
-		else ctx.setKey('mood', mood);
+		else ctx.setKey('mood', typeof mood === 'object' ? mood[lang] : mood);
 
 		clear(() => {
 			ctx.setKey('mood', '');
