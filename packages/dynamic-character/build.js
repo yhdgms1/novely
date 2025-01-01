0d02f48d2dd4abd77d47aa2a67a05713ad3109be
Artemiy Schukin <shuckinart@yandex.ru>
2025-01-01 23:36:42 +0300

    fix

--- packages/dynamic-character/build.js
+++ packages/dynamic-character/build.js
@@ -8,7 +8,7 @@ const dev = process.argv.at(2) === '--w';
 
 const context = await esbuild.context({
 	entryPoints: ['./src/index.ts', './src/index.css'],
-	external: ['@slidy/core'],
+	external: ['solid-js', '@slidy/core'],
 	charset: 'utf8',
 	jsx: 'preserve',
 	platform: 'browser',
